431525778713c175150ddcc4fa097e2ea49f73be
Hariharan <hariharan9572@gmail.com>
2025-08-29 06:45:44 +0530

    Initial commit - all Laravel projects combined

---
+++ event-management/app/Http/Controllers/Api/AttendeeController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\AttendeeResource;
+use App\Http\Traits\CanLoadRelationships;
+use App\Models\Attendee;
+use App\Models\Event;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
+
+class AttendeeController extends Controller
+{
+    use CanLoadRelationships;
+
+    private array $relations = ['user'];
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function __construct(){
+        $this->middleware('auth:sanctum')->except(['index', 'show', 'update']);
+        $this->middleware('throttle:api')
+            ->only('store', 'destroy');
+        $this->authorizeResource(Attendee::class, 'attendee');
+    }
+
+    public function index(Event $event)
+    {
+        $attendee = $this->loadRelationships(
+            $event->attendees()->latest()
+        );
+
+        return AttendeeResource::collection(
+            $attendee->paginate()
+        );
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request, Event $event)
+    {
+        $attendee = $this->loadRelationships(
+            $event->attendees()->create([
+            'user_id' => $request->user()->id
+            ])
+        );
+
+        return new AttendeeResource($attendee);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Event $event, Attendee $attendee)
+    {
+        return new AttendeeResource(
+            $this->loadRelationships($attendee)
+        );
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Event $event, Attendee $attendee)
+    {
+        // if(Gate::denies('delete-attendee', [$event, $attendee])){
+        //     return abort(403, 'Your are not authorized to update this attendee.');
+        // }
+
+        // $this->authorize('delete-attendee', [$event, $attendee]);
+
+        $attendee->delete();
+
+        return response(status: 204);
+    }
+}
---
+++ job-board/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Employer;
+use App\Models\Job;
+use App\Models\JobApplication;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::factory()->create([
+            'name' => 'Hari',
+            'email' => 'hari@gmail.com',
+        ]);
+
+        User::factory(300)->create();
+
+        $users = User::all()->shuffle();
+
+        for($i=0; $i<20; $i++){
+            Employer::factory()->create([
+                'user_id' => $users->pop()->id
+            ]);
+        }
+
+        $employers = Employer::all();
+
+        for($i=0; $i<100; $i++) {
+            Job::factory()->create([
+                'employer_id' => $employers->random()->id
+            ]);
+        }
+
+        foreach($users as $user) {
+            $jobs = Job::inRandomOrder()->take(rand(0,4))->get();
+
+            foreach($jobs as $job) {
+                JobApplication::factory()->create([
+                    'job_id' => $job->id,
+                    'user_id' => $user->id
+                ]);
+            }
+        }
+
+
+    }
+}
